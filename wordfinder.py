7855759fceabbc9e509b7084ebc5bad520eabb2f
Axa Pickering <axapickering@gmail.com>
2023-09-26 16:02:29 -0700

    almost finished with RandomWordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,7 +1,7 @@
 from random import choice
 
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a file."""
 
 
     def __init__(self, path):
@@ -24,3 +24,27 @@ class WordFinder:
             content = file.read()
             content.replace('\n', ' ')
             self.word_list = content.split()
+            print(f"len(self.word_list) words read")
+        file.close()
+
+class RandomWordFinder(WordFinder):
+    '''subclass of WordFinder that can handle empty lines and comments'''
+
+    def __init__(self,path):
+        '''creates a new instance of RandomWordFinder, requires path'''
+        super().__init__(path)
+
+    def get_word_list(self):
+        """creates a list of words from the path """
+        word_list = []
+        with open(self.path, 'r') as file:
+            for line in file:
+                line.replace('\n',"")
+                if (len(line.strip()) != 0 or line.find("#") == -1):
+                    for word in line.split():
+                        word_list.append(word)
+
+        self.word_list = word_list
+        print(f"{len(word_list)} words read")
+        file.close()
+
