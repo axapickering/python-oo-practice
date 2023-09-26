1db38cce5fd280d9eb46cc1745b8313760dc9916
Silu Hu <erinh816@gmail.com>
2023-09-26 19:45:19 -0400

    almost done

--- wordfinder.py
+++ wordfinder.py
@@ -8,6 +8,7 @@ class WordFinder:
         """ construct a word finder"""
         self.path = path
         self.get_word_list()
+        self.print_word_count()
 
     def __repr__(self):
         return f"<Word Finder path={self.path}>"
@@ -15,36 +16,40 @@ class WordFinder:
 
     def random(self):
         """returns a random word from word_list"""
+
         return choice(self.word_list)
 
-
-    def get_word_list(self):
-        """creates a list of words from the path """
-        with open(self.path, 'r') as file:
-            content = file.read()
-            content.replace('\n', ' ')
-            self.word_list = content.split()
-            print(f"len(self.word_list) words read")
-        file.close()
-
-class RandomWordFinder(WordFinder):
-    '''subclass of WordFinder that can handle empty lines and comments'''
-
-    def __init__(self,path):
-        '''creates a new instance of RandomWordFinder, requires path'''
-        super().__init__(path)
 
     def get_word_list(self):
         """creates a list of words from the path """
         word_list = []
         with open(self.path, 'r') as file:
             for line in file:
-                line.replace('\n',"")
-                if (len(line.strip()) != 0 or line.find("#") == -1):
-                    for word in line.split():
-                        word_list.append(word)
+                line = line.strip()
+                word_list.append(line)
+        self.word_list = word_list
+
+        file.close()
+
+
+    def print_word_count(self):
+        """print out the count of words"""
+
+        print(f"{len(self.word_list)} words read")
+
+class RandomWordFinder(WordFinder):
+    '''subclass of WordFinder that can handle empty lines and comments'''
+
+    def get_word_list(self):
+        """creates a list of words from the path """
+        word_list = []
+        super().get_word_list()
+
+        # for line in self.word_list:
+            # if line.startswith('#')
+
 
         self.word_list = word_list
         print(f"{len(word_list)} words read")
         file.close()
-
+        #TODO:shouldn't open the file again/start with what the parent
